318e49435565a024ee4968ac739c0c39d0d5c727
Kinix3331 <kinix3331@gmail.com>
2026-06-24 15:23:19 +0200

    dodanie Models oraz Data a także początkowe utworzenie Bazy Danych

--- EcommerceApi/obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
+++ EcommerceApi/obj/Debug/net8.0/EcommerceApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5640324644e0d623a317012e933086ed0d6032f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c684fca62034357f8710462ed2a59ed3249aa87b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
